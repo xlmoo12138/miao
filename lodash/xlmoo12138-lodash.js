835a08b4bcbb07a432c5211127d086720390df36
limengxu <1354786446@qq.com>
2022-09-14 18:23:44 +0800

    lodash

--- lodash/xlmoo12138-lodash.js
+++ lodash/xlmoo12138-lodash.js
@@ -1,8 +1,22 @@
 var xlmoo12138 = {
-  chunk: function (ary, size) {
+  chunk: function (ary, size = 1) {
+    var result = []
 
+    var count = 0
+    var temp = []
+    for (var i = 0; i < ary.length; i++) {
+      temp.push(ary[i])
+      count++
+      if (count == size || i == ary.length - 1) {
+        result.push(temp)
+        count = 0
+        temp = []
+      }
+    }
+    return result
   },
 
   compact: function() {},
-  difference: function() {},
+  difference: function () { },
+  fill: function() {},
 }
